5758b66b479af0edc583cd43d52957e237d9ae80
Hiruy Legesse Adane <hiruyadane@gmail.com>
2025-11-04 11:32:33 -0800

    Delete Desktop directory

--- Desktop/video generator/ai-video-generator/configs/schema.js
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, varchar, boolean } from "drizzle-orm/pg-core";
-export const User =pgTable('users',{
-    id: serial('id').primaryKey(),
-    name:varchar('name').notNull(),
-    email: varchar('email').notNull(),
-    imgUrl: varchar('imgUrl'),
-    subscription: boolean('subscription').default(false)
-})
--- Desktop/video generator/ai-video-generator/drizzle.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./configs/schema.js",
-  out: "./drizzle",
-    dbCredentials: {
-    url: 'postgresql://neondb_owner:npg_5WaBzyelKEQ3@ep-rapid-bush-adhw8bcw-pooler.c-2.us-east-1.aws.neon.tech/ai-video-gnerator?sslmode=require&channel_binding=require' , // use .env for security
-  },
-});
